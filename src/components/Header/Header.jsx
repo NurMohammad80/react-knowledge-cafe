ae2c931eba4f723ee09aaf6aa6579a877b2b7c25
NurMohammad80 <jorinabagom618@gmail.com>
2023-09-13 10:57:04 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Profile from '../../assets/image/profile.png'
+const Header = () => {
+    return (
+        <div className='flex justify-between items-center p-4 border-b-2'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={Profile} alt="" />
+        </div>
+    );
+};
+
+export default Header;
